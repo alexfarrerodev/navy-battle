31fd7923a765dfaeaf8e7063133c42b82c0af706
cacara82 <cacara890@gmail.com>
2025-02-19 20:22:17 +0100

    added routerlink

--- navy-battle-angular/src/app/app.component.ts
+++ navy-battle-angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
