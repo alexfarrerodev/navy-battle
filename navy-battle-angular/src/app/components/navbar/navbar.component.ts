f9305ff1c84c4c2e9c2a37c902bd6f1eec296bbd
cacara82 <cacara890@gmail.com>
2025-02-19 20:57:50 +0100

    first navbar schema

--- navy-battle-angular/src/app/components/navbar/navbar.component.ts
+++ navy-battle-angular/src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  ngOnInit() {
+    
+    const menuToggle = document.getElementById('menu-toggle');
+    const navbarMenu = document.getElementById('navbar-menu');
+
+    if (menuToggle && navbarMenu) {
+      menuToggle.addEventListener('click', () => {
+        navbarMenu.classList.toggle('hidden');
+      });
+    }
+
+    
+    const dropdownToggle = document.getElementById('dropdownNavbarLink');
+    const dropdownMenu = document.getElementById('dropdownNavbar');
+
+    if (dropdownToggle && dropdownMenu) {
+      dropdownToggle.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('hidden');
+      });
+    }
+  }
+
 }
