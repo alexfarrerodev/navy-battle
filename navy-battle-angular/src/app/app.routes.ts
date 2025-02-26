987ca1f82f74e28852bbb16fff8f49b36b228fa2
cacara82 <cacara890@gmail.com>
2025-02-26 21:13:32 +0100

    changed styles of register and login

--- navy-battle-angular/src/app/app.routes.ts
+++ navy-battle-angular/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './components/home/home.component';
 import { Nf404Component } from './components/nf404/nf404.component';
 import { AboutComponent } from './components/about/about.component';
 import { GamesComponent } from './components/games/games.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: "home", pathMatch: "full"},
     {path: "home", component: HomeComponent},
     {path: "about", component: AboutComponent},
     {path: "games", component: GamesComponent},
+    {path: "register", component: RegisterFormComponent},
+    {path: "login", component: LoginFormComponent},
     {path: "**", component: Nf404Component}
 ];
