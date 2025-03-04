337d41331420aa928c82cd8c766c7689eaa851c8
cacara82 <cacara890@gmail.com>
2025-03-04 20:28:09 +0100

    added about page

--- navy-battle-angular/src/app/components/about/about.component.ts
+++ navy-battle-angular/src/app/components/about/about.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
-  selector: 'app-about',
-  imports: [],
+  selector: 'app-battleship-about',
   templateUrl: './about.component.html',
-  styles: ``
+  animations: [
+    trigger('dropdownAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scaleY(0)' }),
+        animate('300ms ease-out', 
+          style({ opacity: 1, transform: 'scaleY(1)' })
+        )
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', 
+          style({ opacity: 0, transform: 'scaleY(0)' })
+        )
+      ])
+    ])
+  ]
 })
-export class AboutComponent {
 
+export class AboutComponent {
+  showInstructions = false;
+
+  toggleInstructions() {
+    this.showInstructions = !this.showInstructions;
+  }
 }
